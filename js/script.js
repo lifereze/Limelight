41514651f773a55a78b2fa6898254d2b6a3a6b96
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-05-21 02:10:14 +0300

    add alert message to the user when the key in incorrect items

--- js/script.js
+++ js/script.js
@@ -1,21 +1,75 @@
-// START of jquery code for trendingProjects CAROUSEL
-/ Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-});
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+const username = document.getElementByclassName ('')
+const email = document.getElementByclassName ('')
+const password = document.getElementsByClassName ('')
 
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
-// END of jquery code for trendingProjects CAROUSEL
+form.addEventListener('submit',(event) => {
+
+    if (username.value === '' || username.value === null ) {
+        console.log('please enter your username')
+    } 
+     
+    if (password.length <= 6) {
+        console.log('password must be longer than 6 character')
+    } 
+    else if (password.length >= 20) {
+        console.log('password must be less than 20 characters')
+    }
+    else (password.value === 'password') {
+        console.log('Passoward cannot be password')  
+    }
+    
+    
+    function validate(email)  
+    {
+     var gmail = [];
+     for (var i = 0; i < email.length; i++) {
+         var charr = email.charAt(i);
+         if (charr == '@') {
+            i++;
+            for (var j = 0; j < 5; j++) {
+                gmail.push(email.charAt(i));
+                i++;
+            }
+            var checkGmail = gmail.join('');
+            if (checkGmail != 'gmail') {
+                console.log("Please make sure you have the correct email!");
+            } 
+            else {
+                console.log("Good job");
+            }
+          }   
+        }        
+    }
+
+
+    
+    
+
+
+
+   event.preventDefault() 
+   }); 
+   // START of jquery code for trendingProjects CAROUSEL
+   / Instantiate the Bootstrap carousel
+ $('.multi-item-carousel').carousel({
+    interval: false
+ });
+ // for every slide in carousel, copy the next slide's item in the slide.
+ // Do the same for the next, next item.
+ $('.multi-item-carousel .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+ });
+ // END of jquery code for trendingProjects CAROUSEL
+
+
+
