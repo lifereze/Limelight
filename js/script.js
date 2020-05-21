c6318cb9437dfc69698995312e5e8833e51c2c87
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-21 14:23:38 +0300

    add more styles to the carousel, projects now scrollable using the pointer arrows - previous and next

--- js/script.js
+++ js/script.js
@@ -1,22 +1,12 @@
 
 // START of jquery code for trendingProjects CAROUSEL
-/ Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-    interval: false
-});
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function() {
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    if (next.next().length > 0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
 });
 // END of jquery code for trendingProjects CAROUSEL
 
